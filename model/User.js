f0c6db1c25fadf61a4ebd4b7b309e2223595ebe6
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-02-04 10:09:39 +0530

    User schema created

---
+++ model/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ROLES_LIST = require('../config/rolesList');
+
+const userSchema = new Schema({
+    username : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+    },
+    roles : {
+        Admin : {
+            type : Number,
+            default : ROLES_LIST.Admin 
+        },
+        Editor : Number
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    refreshToken : String
+});
+
+const Users = mongoose.model('User', userSchema);
+
+module.exports = {
+    Users
+};
