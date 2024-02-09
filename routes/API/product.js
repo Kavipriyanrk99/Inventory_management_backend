0de5106d1d850bed965d8142c3e0eb1c3de8eb30
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-02-09 22:08:36 +0530

    roles verification middleware added

--- routes/API/product.js
+++ routes/API/product.js
@@ -3,24 +3,26 @@ const router = express.Router();
 const newProductController = require('../../controllers/newProductController');
 const inboundController = require('../../controllers/inboundController');
 const outboundController = require('../../controllers/outboundController');
+const { verifyRoles } = require('../../middleware/verifyRoles');
+const { ROLES_LIST } = require('../../config/rolesList');
 
 router.route('/')
     .get(newProductController.getAllProducts);
 
 router.route('/newproduct')
-    .post(newProductController.createNewProduct)
-    .patch(newProductController.updateProduct)
+    .post(verifyRoles(ROLES_LIST.Admin), newProductController.createNewProduct)
+    .patch(verifyRoles(ROLES_LIST.Admin), newProductController.updateProduct)
 
 router.route('/newproduct/:productID')
-    .delete(newProductController.deleteProduct);
+    .delete(verifyRoles(ROLES_LIST.Admin), newProductController.deleteProduct);
 
 router.route('/:productID')
     .get(newProductController.getProduct);
 
 router.route('/inbound')
-    .patch(inboundController.updateInbound);
+    .patch(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), inboundController.updateInbound);
 
 router.route('/outbound')
-    .patch(outboundController.updateOutbound);
+    .patch(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), outboundController.updateOutbound);
 
 module.exports = router;
